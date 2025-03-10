09fcc2cbe8250935ab138473f6cf2ed5b7986021
tchos <kwenol@yahoo.fr>
2025-03-10 08:31:30 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/geekinstitut/smartmoney/SmartmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.geekinstitut.smartmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekinstitut/smartmoney/SmartmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekinstitut.smartmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
